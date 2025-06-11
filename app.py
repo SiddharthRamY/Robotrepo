e693cb9d129a096d1b99438423bb3e3b65aae621
SiddharthRamY <145451338+SiddharthRamY@users.noreply.github.com>
2025-06-11 15:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import render_template, Flask
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('app.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        if frame is None:
+            continue
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
